f4444f4f60d1a76f4a72fc44106f44738bd82959
masaki <robomasaki2@gmail.com>
2021-05-30 17:35:33 +0900

    feat: attendance
    出席の一覧を取得できるように機能を追加

--- app/application.py
+++ app/application.py
@@ -1,9 +1,11 @@
 from flask import Flask
-from app.views import (bluetooth, index, user)
+
+from app.views import (bluetooth, index, user, attendance)
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
+    app.register_blueprint(attendance.route)
     app.register_blueprint(bluetooth.route)
     app.register_blueprint(index.route)
     app.register_blueprint(user.route)
--- app/domain/repository/device_state_repository.py
+++ app/domain/repository/device_state_repository.py
@@ -1,20 +1,52 @@
+from __future__ import annotations
+
 import time
 from abc import abstractmethod
-from typing import List
+from typing import List, Optional
 
 from app.domain.models.bluetooth import DeviceState
 
 
 class DeviceStateEntity:
-    def __init__(self, address: str, state: DeviceState, created_at: float = time.time()):
-        self.address = address
-        self.state = state
-        self.created_at = created_at
+    def __init__(self, address: str, state: DeviceState):
+        self.created_at = time.time()
+        self.address: str = address
+        self.state: DeviceState = state
+
+    def to_json(self):
+        return {
+            "address": self.address,
+            "state": self.state.name,
+            "createdAt": int(self.created_at),
+        }
+
+    def next(self, states: List[DeviceStateEntity]) -> Optional[DeviceStateEntity]:
+        after = [state for state
+                 in states
+                 if state.created_at > self.created_at
+                 and state.address == self.address
+                 and state.state.value != self.state.value
+                 ]
+        if len(after) == 0:
+            return None
+
+        return after[0]
 
 
 class AbstractDeviceStateRepository:
     @abstractmethod
     async def find_all(self) -> List[DeviceStateEntity]:
+        pass
+
+    @abstractmethod
+    async def find_last(self, address: str) -> Optional[DeviceStateEntity]:
+        """
+        更新日が最新の DeviceStateEntity を取得
+        """
+        pass
+
+    @abstractmethod
+    async def find_all_by_address(self, address: str) -> List[DeviceStateEntity]:
         pass
 
     @abstractmethod
