a6199ebbac853a1d564b239e36d1fdbb718cf220
masaki <robomasaki2@gmail.com>
2021-05-30 17:36:09 +0900

    refactor: get single result
    非同期処理の結果をひとつだけ受け取れるように修正

--- app/infra/bluetooth.py
+++ app/infra/bluetooth.py
@@ -13,7 +13,7 @@ class ScanDeviceResult:
 async def scan_device(address: str) -> ScanDeviceResult:
     loop = asyncio.get_running_loop()
     executor = ProcessPoolExecutor()
-    result = await asyncio.gather(loop.run_in_executor(executor, bluetooth.lookup_name, address, 3))
+    result = await asyncio.ensure_future(loop.run_in_executor(executor, bluetooth.lookup_name, address))
     executor.shutdown(wait=True)
     device_name = result
     return ScanDeviceResult(address=address, found=device_name is not None)
