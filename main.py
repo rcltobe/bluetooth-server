d5396c1480744c46243fb79a0fe3d69ecd21519f
masaki <robomasaki2@gmail.com>
2021-05-30 17:40:49 +0900

    feat: scan devices
    サーバーの起動と同時に、Bluetooth端末の検索を10秒ごと行われるように機能を追加

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+import asyncio
+import threading
+import time
+
 from app.application import create_app
+from app.domain.service.bluetooth import BluetoothService
+
+
+def scan_devices():
+    """
+    10秒ごとにBluetooth端末の検索を行う
+    """
+
+    async def _scan_devices():
+        while True:
+            service = BluetoothService()
+            await service.scan_devices(None)
+            time.sleep(10)
+
+    asyncio.run(_scan_devices())
+
 
 if __name__ == '__main__':
+    t = threading.Thread(target=scan_devices)
+    t.start()
     app = create_app()
-    app.run(port=8080)
+    app.run()
