b60c453208bae2ee8646ec4a23065fc2524334e2
ishizakamasaki <robomasaki2@gmail.com>
2021-06-18 22:38:41 +0900

    feat: changed csv to sqlite repository

--- app/infra/repository.py
+++ app/infra/repository.py
@@ -1,9 +1,9 @@
 from app.domain.repository.device_repository import AbstractDeviceRepository
 from app.domain.repository.device_state_repository import AbstractDeviceStateRepository
 from app.infra.csv.device_repository import CsvDeviceRepository
-from app.infra.csv.device_state_repository import CsvDeviceStateRepository
+from app.infra.sqlite.device_state_repository import SqliteDeviceStateRepository
 
 
 class RepositoryContainer:
     device_repository: AbstractDeviceRepository = CsvDeviceRepository()
-    device_state_repository: AbstractDeviceStateRepository = CsvDeviceStateRepository()
+    device_state_repository: AbstractDeviceStateRepository = SqliteDeviceStateRepository()
