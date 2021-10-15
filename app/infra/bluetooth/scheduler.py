5ab2cd077beade090c78e8ea5056b6bc73fb2b2d
ishizakamasaki <robomasaki2@gmail.com>
2021-10-15 15:46:55 +0900

    feat: added scan scheduler
    端末スキャンをするクラスを作成した。

---
+++ app/infra/bluetooth/scheduler.py
@@ -0,0 +1,55 @@
+import asyncio
+import logging
+import time
+
+from app.domain.service.device import DeviceService
+
+
+class BluetoothScanScheduler:
+    """
+    一定時間ごと、付近の端末をスキャンし、その結果を保存する。
+    """
+
+    # スキャン処理を行う間隔
+    INTERVAL_SCAN = 60
+
+    SECONDS_HOUR = 60 * 60
+    SECONDS_DAY = 24 * 60 * 60
+
+    # 1日分のスキャン結果を残す
+    SAVE_RESULT_SPAN = 1 * SECONDS_DAY
+
+    def __init__(self):
+        self.service = DeviceService()
+
+    def run_sync(self):
+        asyncio.run(self.run())
+
+    async def run(self):
+        next_delete = time.time() + self.SECONDS_HOUR
+        while True:
+            await self._scan_devices(self.INTERVAL_SCAN)
+
+            # 1時間ごとに、今から2日前の出席データを削除する
+            now = time.time()
+            if now < next_delete:
+                await self.service.delete_results_before(time.time() - self.SAVE_RESULT_SPAN)
+                next_delete += self.SECONDS_HOUR
+
+    async def _scan_devices(self, interval: int):
+        logging.info("START SCAN")
+        start = time.time()
+
+        try:
+            await self.service.scan_devices()
+        except Exception as e:
+            logging.error(e)
+
+        logging.info("END SCAN")
+        end = time.time()
+
+        # 1回のサイクルに必ず一定時間かけることで、
+        # 不必要な繰り返しをしないようにする
+        if end - start < interval:
+            duration = end - start
+            time.sleep(interval - duration)
--- main.py
+++ main.py
@@ -1,57 +1,14 @@
-import asyncio
 import logging
 import os
 import threading
-import time
 
 import dotenv
 
 from app.application import create_app
-from app.domain.service.device import DeviceService
+from app.infra.bluetooth.scheduler import BluetoothScanScheduler
 
 
-def scan_devices():
-    """
-    10秒ごとにBluetooth端末の検索を行う
-    """
-    # スキャン処理を行う間隔
-    interval_scan = 60
-
-    # 2日分のスキャン結果を残す
-    mills_day = 24 * 60 * 60 * 1000
-    save_result_span = 2 * mills_day
-
-    async def _scan_devices():
-        logging.info("START SCAN")
-
-        counter = 0
-        while True:
-            start = time.time()
-            service = DeviceService()
-            try:
-                await service.scan_devices(None)
-            except Exception as e:
-                logging.error(e)
-
-            end = time.time()
-
-            # 1回のサイクルに最低60秒かかるようにし、
-            # 不必要な繰り返しをしないようにする
-            if end - start < interval_scan:
-                duration = end - start
-                time.sleep(interval_scan - duration)
-
-            # 1時間ごとに、今から2日前の出席データを削除する
-            if counter == 60:
-                await service.delete_results_before(time.time() - save_result_span)
-                counter = 0
-
-            counter += 1
-
-    asyncio.run(_scan_devices())
-
-
-if __name__ == '__main__':
+def main():
     # ロガーの初期化
     logging.basicConfig(format='%(asctime)s[%(levelname)s]: %(message)s', level=logging.INFO)
 
@@ -61,9 +18,14 @@ if __name__ == '__main__':
     dotenv.load_dotenv(dotenv_path)
 
     # 端末のスキャンを開始
-    t = threading.Thread(target=scan_devices)
+    scheduler = BluetoothScanScheduler()
+    t = threading.Thread(target=scheduler.run_sync)
     t.start()
 
     # サーバーを起動
     app = create_app()
     app.run(host="0.0.0.0")
+
+
+if __name__ == '__main__':
+    main()
