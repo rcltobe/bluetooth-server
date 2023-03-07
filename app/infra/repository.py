46c7009832f94d2bf9ff89728dca0d94bcedf02d
ishizakamasaki <robomasaki2@gmail.com>
2023-03-07 15:36:04 +0900

    refactor: removed device state

--- app/infra/repository.py
+++ app/infra/repository.py
@@ -1,9 +1,6 @@
 from app.domain.repository.user_repository import AbstractUserRepository
-from app.domain.repository.device_state_repository import AbstractDeviceStateRepository
 from app.infra.spreadsheet.user_repository import SpreadSheetUserRepository
-from app.infra.spreadsheet.device_state_repository import SpreadSheetDeviceStateRepository
 
 
 class RepositoryContainer:
     device_repository: AbstractUserRepository = SpreadSheetUserRepository()
-    device_state_repository: AbstractDeviceStateRepository = SpreadSheetDeviceStateRepository()
