d6d0903769862cbd9d930c9817892a485daca1c0
ishizakamasaki <robomasaki2@gmail.com>
2023-03-07 15:33:00 +0900

    feat: Attendance Log形式でデータを保存するように修正

--- app/domain/service/device.py
+++ app/domain/service/device.py
@@ -2,11 +2,12 @@ import logging
 import time
 from typing import Optional
 
-from app.domain.models.device_state import DeviceState
+from app.domain.models.attendance_log import AttendanceLog
+from app.domain.models.user import User
 from app.domain.repository.user_repository import AbstractUserRepository
-from app.domain.repository.device_state_repository import AbstractDeviceStateRepository
 from app.infra.bluetooth.scanner import scan_device
 from app.infra.repository import RepositoryContainer
+from app.infra.spreadsheet.attendance_log_repository import SpreadSheetAttendanceLogRepository
 
 
 class DeviceService:
@@ -17,18 +18,12 @@ class DeviceService:
 
     def __init__(self,
                  user_repository: AbstractUserRepository = RepositoryContainer.device_repository,
-                 state_repository: AbstractDeviceStateRepository = RepositoryContainer.device_state_repository,
                  ):
+        self.attendance_log_repository = SpreadSheetAttendanceLogRepository()
         self.user_repository = user_repository
-        self.state_repository = state_repository
         self.logger = logging.getLogger(__name__)
 
-    INTERVAL_SCAN = 10 * 60  # 端末を発見してから次に検索するまでの間隔
     INTERVAL_UPDATE = 10 * 60  # 状態が変化していないときに、更新する間隔
-
-    async def delete_results_before(self, time_in_mills):
-        self.logger.info(f"delete result before {time_in_mills}")
-        await self.state_repository.delete_before(time_in_mills)
 
     async def scan_devices(self):
         """
@@ -38,52 +33,54 @@ class DeviceService:
         この処理を同時に呼ぶと、正しい結果を得ることができない
         """
         # 登録されたすべての端末のMACアドレスを取得
-        devices = await self.user_repository.find_all()
-        addresses = [device.address for device in devices]
+        users = await self.user_repository.find_all()
+        addresses = [device.address for device in users]
 
         self.logger.info(f"SCAN FOR {addresses}")
 
+        # 本日分のAttendance Logを取得する
+        attendance_logs_today = await self.attendance_log_repository.fetch_logs_of_today()
+        bluetooth_address_to_log = {log.bluetooth_mac_address: log for log in attendance_logs_today}
+
         # 端末をBluetoothでスキャンする
-        all_states = await self.state_repository.find_all()
-        scan_results = []
-        for address in addresses:
-            last_state = DeviceState.get_last_device_states(address, all_states)
-            result = await self._scan_device(address, last_state)
-            if result is not None:
-                scan_results.append(result)
+        attendance_logs = []
+        for user in users:
+            prev_log: Optional[AttendanceLog] = bluetooth_address_to_log.get(user.address)
+            try:
+                attendance_log = self._scan_device(user=user, prev_attendance_log=prev_log)
+                attendance_logs.append(attendance_log)
+            except Exception as e:
+                logging.error(e)
+                if prev_log is None:
+                    continue
+                attendance_logs.append(prev_log)
 
-        # 保存が必要な結果のみを抽出
-        results_for_updates = []
-        for result in scan_results:
-            prev_state = DeviceState.get_last_device_states(result.address, all_states)
-            if result.should_update_state(prev_state):
-                p = f"{prev_state.found}" if prev_state else "NONE"
-                self.logger.info(f"UPDATE {result.address}({p} -> {result.found})")
-                results_for_updates.append(result)
+        # attendance logを更新する
+        await self.attendance_log_repository.update_logs_today(attendance_logs)
 
-        # スキャン結果を保存
-        await self.state_repository.save_all(results_for_updates)
-
-    async def _scan_device(self, address: str, prev_state: Optional[DeviceState]) -> Optional[DeviceState]:
+    async def _scan_device(
+            self,
+            user: User,
+            prev_attendance_log: Optional[AttendanceLog]
+    ) -> AttendanceLog:
         """
         付近に端末がいるかどうか、スキャンする
         @:param prev_state 前回のスキャン結果
         @:return スキャン結果（10分以内に発見されている場合はNoneを返す）
         """
-        # 10分以内に発見されていたら、スキャンせずに、前の結果を使う
-        if prev_state is not None \
-                and prev_state.created_at >= time.time() - self.INTERVAL_SCAN \
-                and prev_state.found:
-            self.logger.info(f"SCAN(USE CACHE) {prev_state.to_json()}")
-            return None
-
         # スキャン
-        try:
-            result = scan_device(address=address)
-        except Exception as e:
-            logging.error(e)
-            return None
+        result = scan_device(address=user.address)
 
-        prev_state = DeviceState(address=address, found=result.found)
-        self.logger.info(f"DEVICE SCANNED {prev_state.to_json()}")
-        return prev_state
+        if prev_attendance_log is None:
+            attendance_log = AttendanceLog(
+                user_id=user.user_id,
+                bluetooth_mac_address=user.address,
+                in_at=int(time.time()),
+                out_at=None,
+            )
+        else:
+            prev_attendance_log.update_log(result.found)
+            attendance_log = prev_attendance_log
+
+        self.logger.info(f"DEVICE SCANNED {attendance_log.to_json()}")
+        return attendance_log
