6605968a4252ffef7f2880a12533b1fd7f7ef052
zacker <iszkmasaki@gmail.com>
2024-03-28 10:13:50 +0900

    feat: データの取得処理を実装

--- app/infra/firestore/attendance_log_repository.py
+++ app/infra/firestore/attendance_log_repository.py
@@ -3,6 +3,7 @@ from typing import List
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+from google.cloud.firestore_v1.base_query import FieldFilter
 
 from app.domain.models.attendance_log import AttendanceLog
 
@@ -13,8 +14,26 @@ class FirestoreAttendanceLogRepository:
         self.app = firebase_admin.initialize_app(cred)
         self.db = firestore.client()
 
-    async def fetch_logs_of_today(self) -> List[AttendanceLog]:
-        pass
+    async def fetch_logs_of_day(self, day: datetime) -> List[AttendanceLog]:
+        day_start = datetime(day.year, day.month, day.day, 0, 0, 0)
+        day_end = datetime(day.year, day.month, day.day, 23, 59, 59)
+        query = self.db.collection('attendance')
+        query = query.where(filter=FieldFilter('created_at', '>=', day_start))
+        query = query.where(filter=FieldFilter('created_at', '<=', day_end))
+        docs = query.stream()
+        logs = []
+        for doc in docs:
+            data = doc.to_dict()
+            logs.append(AttendanceLog(
+                user_id=data['user_id'],
+                user_name=data['user_name'],
+                bluetooth_mac_address=data['address'],
+                is_attending=data['is_attending'],
+                room=data['room'],
+                created_at=data['created_at'].timestamp()
+            ))
+        
+        return logs
 
     async def add_attendance_logs(self, attendance_logs: List[AttendanceLog]):
         for log in attendance_logs:
@@ -22,7 +41,7 @@ class FirestoreAttendanceLogRepository:
                 'user_id': log.user_id,
                 'user_name': log.user_name,
                 'address': log.bluetooth_mac_address,
-                'created_at': datetime.fromtimestamp(log.created_at),
                 'is_attending': log.is_attending,
-                'room': log.room
+                'room': log.room,
+                'created_at': datetime.fromtimestamp(log.created_at),
             })
